7fb44dd775a1e517c4abe5507a2d2a49a589a52e
Maham.J <139694482+Maham-j@users.noreply.github.com>
2024-10-18 22:27:45 +0500

    Create integer.cpp

---
+++ Data Types and Variables /integer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Variable declarations
+    int a, b, c;
+    short _sa = 9; // Short integer, typically 2 bytes
+    cout << _sa << endl;
+
+    short a; // Short integer, typically 2 bytes
+    int b; // Integer, typically 4 bytes
+    long c; // Long integer, typically 4 or 8 bytes (depends on the system)
+    long long d; // Long long integer, typically 8 bytes
+
+    
+    return 0; // End the program and return 0
+}
