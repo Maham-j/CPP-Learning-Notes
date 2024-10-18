94eca5211fc2cbda8579dd309fa99b9c3aeae49c
Maham.J <139694482+Maham-j@users.noreply.github.com>
2024-10-18 19:51:07 +0500

    Create main.cpp

---
+++ Comments/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream> // Include the standard input-output library
+using namespace std; // Use the standard namespace
+
+int main() { 
+    cout << "Hello World"<<endl; // Output "Hello World" to the console
+    cout << "Next line";  // Output in next line
+
+   /* cout << "Hello World"<<endl; 
+    cout << "Next line"; */ 
+  
+    return 0; // Return 0 to signal the program ended successfully
+}
