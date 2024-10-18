3fa71fcd72ac76a82625ddf242afe5c34830919b
Maham.J <139694482+Maham-j@users.noreply.github.com>
2024-10-19 02:24:17 +0500

    Update string.cpp

--- Data Types and Variables /string.cpp
+++ Data Types and Variables /string.cpp
@@ -1,12 +1,16 @@
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
     // String example
-    string studentName = "John Doe"; // Declare and initialize a string
+    string studentName = "Maham Jamil"; // Declare and initialize a string
     cout << "Student Name: " << studentName << endl; // Output the string
-
+    cout << "Length of Student Name is: " << studentName.length() << endl; // Output the string length
+    cout << "Substring of Student Name: " << studentName.substr(0,3) << endl; // Output the substring (starting from index 0  and 3 characters)
+    cout << "Substring of Student Name: " << studentName.substr(2,34) << endl; // Output the substring (starting from index 2 and at the end)
+ 
     return 0; // End the program and return 0
 }
 
