35e9f6a0dfc5db92f6454b9aaeec6b713e1e4686
Maham.J <139694482+Maham-j@users.noreply.github.com>
2024-10-19 00:21:46 +0500

    Create Input array.cpp

---
+++ Arrays/Input array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int arr[6];
+	for (int i=0;i<6;++i){
+		cin>>arr[i];
+	}
+	for (int i=0;i<6;++i){
+		cout<< "Marks of student " <<i<<" is "<<arr[i]<<endl;
+	}
+    	return 0;
+    		
+	}
+
