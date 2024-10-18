1e5be16d50318ac705dd602fe590881b8e1e9662
Maham.J <139694482+Maham-j@users.noreply.github.com>
2024-10-18 22:40:00 +0500

    Create string.cpp

---
+++ Data Types and Variables /string.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // String example
+    string studentName = "John Doe"; // Declare and initialize a string
+    cout << "Student Name: " << studentName << endl; // Output the string
+
+    return 0; // End the program and return 0
+}
+
