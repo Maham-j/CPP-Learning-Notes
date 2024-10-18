d1d220b045a92b6f0620754b0775e63e43e704cc
Maham.J <139694482+Maham-j@users.noreply.github.com>
2024-10-18 23:27:56 +0500

    Create for loop.cpp

---
+++ Loops/for loop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "For Loop: Counting from 1 to 5" << endl;
+    
+    for (int count = 1; count <= 5; count++) {
+        cout << count << endl; // Output current count
+    }
+
+    return 0; // End the program and return 0
+}
