2ee23185d6362b5eaceb96770ed9c13671137b2f
Maham.J <139694482+Maham-j@users.noreply.github.com>
2024-10-18 22:32:48 +0500

    Create constants.cpp

---
+++ Data Types and Variables /constants.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Const example (commented out)
+    const float scoreConst = 43.32f; // Constant float, cannot be modified
+
+    cout << "The score is " << scoreConst << endl;
+
+    return 0; // End the program and return 0
+}
