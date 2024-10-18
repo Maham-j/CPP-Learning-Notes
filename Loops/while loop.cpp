7d19ea4e0d40244a1d86f954021e943d4059f3c4
Maham.J <139694482+Maham-j@users.noreply.github.com>
2024-10-18 23:26:47 +0500

    Create while loop.cpp

---
+++ Loops/while loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int count = 1; // Initialize count
+
+    cout << "While Loop: Counting from 1 to 5" << endl;
+    while (count <= 5) {
+        cout << count << endl; // Output current count
+        count++; // Increment count
+    }
+
+    return 0; // End the program and return 0
+}
