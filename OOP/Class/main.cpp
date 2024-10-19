ec2e5ceb99a1ae5b3ea488859d972386e193ceea
Maham.J <139694482+Maham-j@users.noreply.github.com>
2024-10-19 15:39:02 +0500

    Update main.cpp

--- OOP/Class/main.cpp
+++ OOP/Class/main.cpp
@@ -2,25 +2,30 @@
 #include <string>
 using namespace std;
 
-class Employee{
-	public:
-		string name;
-		int salary;
-		
-	void printDetails(){
-		cout << "The name of employee is " << this->name << " and her salary is " << this->salary <<" Dollars"<<endl;
-	}
+class Employee {
+    public:
+        string name;     // Employee's name
+        int salary;      // Employee's salary
+    	// int salary = 0;  a default value in case no value is set for salary
+
+    // Function to print employee details
+    void printDetails() {
+        cout << "The name of employee is " << this->name << " and her salary is " << this->salary << " Dollars" << endl;
+    }
 };
+
 int main() {
-   
+   // Create and initialize first employee
    Employee emp;
    emp.name = "Maham";
    emp.salary = 100;
-   emp.printDetails();
+   emp.printDetails();   // Prints Maham's details
    
+   // Create and initialize second employee
    Employee emp2;
    emp2.name = "Jamil";
-   emp2.salary = 150;
-   emp2.printDetails();
-    return 0; 
+   emp2.salary = 150; // if no value is set ,will print garbage value
+   emp2.printDetails();  // Prints Jamil's details
+
+   return 0;
 }
